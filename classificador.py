149113df0ec05e5dbc4bea448d3d2e49bf657847
Hugo Guerra <123105204+hugo-guerra@users.noreply.github.com>
2026-06-24 13:46:16 -0300

    Padrões e loop
    Começando a adicionar os padrões e colocando um loop para percorrer cada item e imprimir o nome do dado e o que foi encontrado no conteudo.

--- classificador.py
+++ classificador.py
@@ -3,6 +3,12 @@ import re
 with open("dados/amostra.txt", "r", encoding="utf-8") as arquivo:
     conteudo = arquivo.read()
 
+padroes = {
+    "CPF": r"\d{3}\.\d{3}\.\d{3}-\d{2}",
+    "E-mail": r"\w+@\w+\.\w+",
+    "Telefone": r"\(\d{2}\)\s\d{4,5}-\d{4}"
+}
 
-
-print(conteudo)
+for chave, valor in padroes.items():
+    encontrados = re.findall(valor, conteudo)
+    print(chave, encontrados)
